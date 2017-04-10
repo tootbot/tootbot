8ff079ca7bef5034952abbb8f9fbe771ff2c7c5f
Alexsander Akers <me@a2.io>
2017-04-10 17:38:41 +0200

    Clean up trailing whitespace

--- Tootbot/AppDelegate.swift
+++ Tootbot/AppDelegate.swift
@@ -3,7 +3,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - App Delegate
-    
+
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
