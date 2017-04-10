ec7af802a1f471aefdb04946a8d8eb43d2150bce
Alexsander Akers <me@a2.io>
2017-04-10 17:13:31 +0200

    Initial commit

---
+++ Tootbot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tootbot
+//
+//  Created by Alexsander Akers on 4/10/17.
+//  Copyright © 2017 Pandamonia LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
