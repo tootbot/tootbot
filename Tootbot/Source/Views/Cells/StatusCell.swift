dd348cdec4ffb150f3963cfd9ac70f753a0d3287
Alexsander Akers <me@a2.io>
2017-04-24 19:29:13 +0200

    Add link handling in StatusCell content text view

--- Tootbot/Source/Views/Cells/StatusCell.swift
+++ Tootbot/Source/Views/Cells/StatusCell.swift
@@ -15,10 +15,16 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-import UIKit
-import ReactiveSwift
 import ReactiveCocoa
+import ReactiveSwift
 import Result
+import UIKit
+
+enum LinkType {
+    case hashtagMention
+    case userMention
+    case hyperlink
+}
 
 class StatusCell: UITableViewCell {
     @IBOutlet var avatarImageView: UIImageView!
@@ -29,6 +35,10 @@ class StatusCell: UITableViewCell {
     @IBOutlet var placeholderContentView: UIView!
     var contentTextView: StatusTextView!
 
+    typealias LinkTappedValue = (linkType: LinkType, link: String, boundingRect: CGRect)
+    let linkTappedSignal: Signal<LinkTappedValue, NoError>
+    private let linkTappedObserver: Observer<LinkTappedValue, NoError>
+
     private static let dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .short
@@ -36,6 +46,16 @@ class StatusCell: UITableViewCell {
         dateFormatter.timeStyle = .short
         return dateFormatter
     }()
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        (self.linkTappedSignal, self.linkTappedObserver) = Signal.pipe()
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        (self.linkTappedSignal, self.linkTappedObserver) = Signal.pipe()
+        super.init(coder: aDecoder)
+    }
 
     var viewModel: StatusCellViewModel? {
         didSet {
@@ -85,12 +105,44 @@ class StatusCell: UITableViewCell {
         placeholderContentView.removeFromSuperview()
     }
 
+    private func configureLinkTappedHandler() {
+        let tap = UITapGestureRecognizer()
+        contentContainerStackView.addGestureRecognizer(tap)
+
+        tap.reactive.stateChanged
+            .filter { gesture in gesture.state == .recognized }
+            .map { [unowned self] gesture in gesture.location(in: self.contentTextView) }
+            .map { [unowned self] location -> ([String: Any], CGRect)? in
+                var boundingRect = CGRect()
+                if let attributes = self.contentTextView.attributes(at: location, boundingRect: &boundingRect) {
+                    return (attributes, boundingRect)
+                } else {
+                    return nil
+                }
+            }
+            .skipNil()
+            .map { (attributes, boundingRect) -> LinkTappedValue? in
+                if let hashtag = attributes[HashtagMentionAttributeName] as? String {
+                    return (.hashtagMention, hashtag, boundingRect)
+                } else if let user = attributes[UserMentionAttributeName] as? String {
+                    return (.userMention, user, boundingRect)
+                } else if let link = attributes[NSLinkAttributeName] as? String {
+                    return (.hyperlink, link, boundingRect)
+                } else {
+                    return nil
+                }
+            }
+            .skipNil()
+            .observe(linkTappedObserver)
+    }
+
     // MARK: - Table View Cell
 
     override func awakeFromNib() {
         super.awakeFromNib()
 
         configureContentTextView()
+        configureLinkTappedHandler()
     }
 
     override func prepareForReuse() {
